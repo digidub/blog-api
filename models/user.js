0eed9af5571b15582cd1fb285267c598d0652d1d
Sholto <madein.mania@gmail.com>
2021-10-22 14:03:20 +0100

    fix type string and export model

--- models/user.js
+++ models/user.js
@@ -3,5 +3,7 @@ const Schema = mongoose.schema();
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
-  password: { type: STring, required: true },
+  password: { type: String, required: true },
 });
+
+export default mongoose.model('Post', PostSchema);
