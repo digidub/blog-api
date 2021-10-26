93571193d3ed6d377e1accd12c6daf276d7f92b2
Sholto <madein.mania@gmail.com>
2021-10-26 14:22:58 +0100

    username unique true

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
