68c7c7d3e30d248a51935f07dc5835e9a8eacc5c
Sholto <madein.mania@gmail.com>
2021-10-26 09:11:41 +0100

    populate username only of author

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,10 @@
 import Post from '../models/posts';
 
 export const get = async (req, res) => {
-  const posts = await Post.find({}, 'title body datePosted author').sort({
-    datePosted: -1,
-  });
+  const posts = await Post.find({}, 'title body datePosted author')
+    .sort({
+      datePosted: -1,
+    })
+    .populate('author', 'username');
   res.json({ posts });
 };
