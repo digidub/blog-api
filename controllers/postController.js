cd2c427c0d1f73c40e80c3c9608a806d1c797702
Sholto <madein.mania@gmail.com>
2021-10-26 09:15:54 +0100

    basic new post control function

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,3 +8,13 @@ export const get = async (req, res) => {
     .populate('author', 'username');
   res.json({ posts });
 };
+
+export const post = async (req, res) => {
+  const newPost = new Post({
+    title: req.body.title,
+    body: req.body.body,
+    author: req.body.author,
+  });
+  await newPost.save();
+  res.json(newPost);
+};
