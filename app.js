1b67bb8e16b0f8b1417075d02241d0a1e88e9a1c
Sholto <madein.mania@gmail.com>
2021-10-26 14:20:57 +0100

    use passport

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import routes from './routes';
+import passport from 'passport';
 
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -10,8 +11,13 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
 
 app.use('/users', routes.user);
+app.use('/posts', routes.post);
 
 app.listen(3000, () => console.log('App listening on port 3000'));
