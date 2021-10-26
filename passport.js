f56b7785fe430f9958ebe1ae7cdc3066450dba65
Sholto <madein.mania@gmail.com>
2021-10-26 14:22:36 +0100

    bcrypt auth

--- passport.js
+++ passport.js
@@ -1,27 +1,25 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import User from './models/users';
+import bcrypt from 'bcryptjs';
 
 passport.use(
-  new LocalStrategy(
-    {
-      username: 'username',
-      password: 'password',
-    },
-    function (username, password, cb) {
-      return User.findOne({ username, password })
-        .then((user) => {
-          if (!user) {
-            return cb(null, false, {
-              message: 'Incorrect username or password.',
-            });
-          }
-
-          return cb(null, user, { message: 'Logged In Successfully' });
-        })
-        .catch((err) => cb(err));
-    }
-  )
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'User Authentication Failed' });
+        }
+      });
+    });
+  })
 );
 
-export default passport;
+export const authLocal = passport.authenticate('local', {
+  session: false,
+});
