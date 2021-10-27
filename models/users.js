6783d597513bd4783a0f2090d4473a943c663418
Sholto <madein.mania@gmail.com>
2021-10-27 09:06:58 +0100

    cleaned up schema

--- models/users.js
+++ models/users.js
@@ -1,29 +1,10 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
-
-// UserSchema.pre('save', (next) => {
-//   const user = this;
-//   if (!user.isModified('password')) {
-//     return next();
-//   }
-//   bcrypt.hash(user.password, 10).then((hashedPassword) => {
-//     user.password = hashedPassword;
-//     next();
-//   });
-// });
-
-// UserSchema.methods.comparePassword = function (password, next) {
-//   bcrypt.compare(password, this.password, function (err, isMatch) {
-//     if (err) return next(err);
-//     next(null, isMatch);
-//   });
-// };
 
 const User = mongoose.model('User', UserSchema);
 
