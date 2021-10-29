4945a2e0e347b65bcaaabb9235f8ff1786a7f385
Sholto <madein.mania@gmail.com>
2021-10-29 10:11:18 +0100

    error handling for post functions

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,41 +2,61 @@ import Post from '../models/posts';
 import User from '../models/users';
 
 export const get = async (req, res) => {
-  const posts = await Post.find({}, 'title body datePosted author')
-    .sort({
-      datePosted: -1,
-    })
-    .populate('author', 'username');
-  res.json({ posts });
+  try {
+    const posts = await Post.find({}, 'title body datePosted author')
+      .sort({
+        datePosted: -1,
+      })
+      .populate('author', 'username');
+    res.json({ posts });
+  } catch (err) {
+    res.json(err);
+  }
 };
 
 export const getID = async (req, res) => {
-  const post = await Post.findById(req.params.postID);
-  res.json({ post });
+  try {
+    const post = await Post.findById(req.params.postID);
+    res.json({ post });
+  } catch (err) {
+    res.json(err);
+  }
 };
 
 export const post = async (req, res) => {
-  const author = await User.findOne({ username: req.user.username });
-  const newPost = new Post({
-    title: req.body.title,
-    body: req.body.body,
-    author: author._id,
-  });
-  await newPost.save();
-  res.json(newPost);
+  try {
+    const author = await User.findOne({ username: req.user.username });
+    const newPost = new Post({
+      title: req.body.title,
+      body: req.body.body,
+      author: author._id,
+    });
+    await newPost.save();
+    res.json(newPost);
+  } catch (err) {
+    res.json(err);
+  }
 };
 
 export const update = async (req, res) => {
-  const post = await Post.findById(req.params.postID);
-  post.title = req.body.title;
-  post.body = req.body.body;
-  post.dateEdited = Date.now();
-  post.save();
-  res.json({ updated: post });
+  try {
+    const post = await Post.findById(req.params.postID);
+    post.title = req.body.title;
+    post.body = req.body.body;
+    post.dateEdited = Date.now();
+    post.save();
+    res.json({ updated: post });
+  } catch (err) {
+    res.json(err);
+  }
 };
 
 export const remove = async (req, res) => {
-  const post = await Post.findById(req.params.postID);
+  try {
+    const post = await Post.findById(req.params.postID);
+  } catch (err) {
+    res.json(err);
+  }
   await Post.findByIdAndDelete(post._id, (err) => {
     if (err) res.json(err);
     res.json({ deleted: post });
